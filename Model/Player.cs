7e848fa3c2a08b6c624af95d0b78b9d3b8739a4e
Emily Orton <eort3611@740s-pl-177.local>
2016-05-10 13:27:40 -0600

    MasterCommi

---
+++ Model/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpaceGame
+{
+	public class Player
+	{
+		private int score;
+
+		public int Score
+		{
+			get {	return score; }
+			set { score = value; }
+		}
+		public Player ()
+		{
+		}
+	}
+}
+
